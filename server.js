00596b688468dc68541f21aa833fe0989932d189
Pratyush Ranjan <118924484+pratyushranjn@users.noreply.github.com>
2025-02-27 19:31:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -81,9 +81,9 @@ main()
 app.use(authRoutes);
 app.use(songRoutes);
 
-// const PORT = process.env.PORT || 3000; // Use Vercel-assigned port
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+  const PORT = process.env.PORT || 3000; // Use Vercel-assigned port
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
